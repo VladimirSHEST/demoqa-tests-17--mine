f5dfbf378dc6bb2e3b8d945aa98919d8b0f0065c
Vladimir <piterskivv@mail.ru>
2024-04-12 18:26:11 +0400

    закончил автотест

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,20 @@
+//package junit;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//public class FirstJUnitTest {
+//    @BeforeEach
+//    void beForeEach(){
+//        System.out.println(" beForeEach");
+//    }
+//    @AfterEach
+//    void afterEach(){
+//        System.out.println(" afterEach");
+//    }
+//    @Test
+//    void firstTest(){
+//        System.out.println("выполняется первый тест");
+//    }
+//}
