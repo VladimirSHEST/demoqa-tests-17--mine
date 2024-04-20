43fc3e7863d0a66fd82b23a4839e448057e74590
VladimirSHEST <piterskiyvv@mail.ru>
2024-04-20 22:54:12 +0400

    сделал дом задание

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -5,11 +5,12 @@ import pages.components.CalendarComponent;
 import pages.components.RegistrationResultsModal;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationPage {
     CalendarComponent calendarComponent = new CalendarComponent();
-    public  RegistrationResultsModal registrationResultsModal = new RegistrationResultsModal();
+    public RegistrationResultsModal registrationResultsModal = new RegistrationResultsModal();
     private final String TITLE_TEXT = "Practice Form";
 
     private SelenideElement
@@ -59,12 +60,52 @@ public class RegistrationPage {
         return this;
     }
 
+    public RegistrationPage setSubject() {    // хобби
+        $x("//input[@id='subjectsInput']").setValue("Maths").pressEnter();
+        return this;
+    }
+
+    public RegistrationPage setHobbies() {
+        $x("//label[@for='hobbies-checkbox-11']").click();
+        return this;
+    }
+
+    public RegistrationPage setAddress() {     // адрес
+        $x("//textarea[@placeholder='Current Address']").setValue("Samara");
+        return this;
+    }
+
+    public RegistrationPage setCity() {     //штат и город
+        $x("//div[@class=' css-1wa3eu0-placeholder']").click();  // клик по кнопке для выпадения
+        $("#stateCity-wrapper").$(byText("NCR")).click();   // клик по тексту через общий див
+        $x("//div[text()='Select City']").click();  // клик по кнопке для выпадения
+        $x("//div[@id='react-select-4-option-2']").click();   // поиск элемента через заморозку
+        return this;
+    }
+
+    public RegistrationPage setUploadPicture() {    // вставка файла
+        $x("//input[@id='uploadPicture']").uploadFromClasspath("img/1.png");
+        return this;
+    }
+
+    public RegistrationPage setSubmit() {  // рассмотрение
+        $("#submit").click();
+        return this;
+    }
+
+
     public RegistrationPage verifyResultsModalAppear() {
         registrationResultsModal.verifyModalAppear();
         return this;
-    }    public RegistrationPage verifyResult(String key, String value) {
+    }
+
+    public RegistrationPage verifyResult(String key, String value) {
         registrationResultsModal.verifyResult(key, value);
         return this;
+    }
+
+    public void verifyTable() {    //  проверка заполнения таблицы
+        $(".table-responsive").shouldHave(text("Vova"), text("Shest"), text("piterskiyvv@mail.ru"));
     }
 
 
--- src/test/java/tests/RegistrationWithPageObjectTest.java
+++ src/test/java/tests/RegistrationWithPageObjectTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
@@ -14,7 +13,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationWithPageObjectTest extends TestBase {
 
-
     @Test
     void FillFormTest() {
         String userName = "Vova";
@@ -22,37 +20,17 @@ public class RegistrationWithPageObjectTest extends TestBase {
 
         registrationPage.openPage().setFirstName(userName)
                 .setLastName("Shest").setEmail("piterskiyvv@mail.ru")
-                .setGender().setNumber("1234567890").setBirthDay();
-
-
-        $x("//input[@id='subjectsInput']").setValue("Maths").pressEnter();  // предметы
-        $x("//label[@for='hobbies-checkbox-1']").click();  // хобби
-        $x("//textarea[@placeholder='Current Address']").setValue("Samara");   // адрес
-
-//      штат и город
-        $x("//div[@class=' css-1wa3eu0-placeholder']").click();  // клик по кнопке для выпадения
-        $("#stateCity-wrapper").$(byText("NCR")).click();   // клик по тексту через общий див
-//        $x("(//div[@class=' css-tlfecz-indicatorContainer'])[1]").click();  // как искать через массив элементов?
-//        $x("//div[@id='react-select-3-option-2']").click();
-        $x("//div[text()='Select City']").click();  // клик по кнопке для выпадения
-        $x("//div[@id='react-select-4-option-2']").click();   // поиск элемента через заморозку
-
-        // вставка файла
-        $x("//input[@id='uploadPicture']").uploadFromClasspath("img/1.png");
-
-        $("#submit").click();     // рассмотрение
+                .setGender().setNumber("1234567890").setBirthDay().setSubject()
+                .setHobbies().setAddress().setCity().setUploadPicture()
+                .setSubmit();
 
         registrationPage.verifyResultsModalAppear()
-                .verifyResult("Student Name","Vova Shest")
-                .verifyResult("Student Email","piterskiyvv@mail.ru")
-                .verifyResult("Subjects","Maths")
-                .verifyResult("Address","Samara");
+                .verifyResult("Student Name", "Vova Shest")
+                .verifyResult("Student Email", "piterskiyvv@mail.ru")
+                .verifyResult("Subjects", "Maths")
+                .verifyResult("Address", "Samara").setCity();
 
-
-
-
-
-        $(".table-responsive").shouldHave(text(userName), text("Shest"), text("piterskiyvv@mail.ru"));  //  проверка заполнения таблицы
+        registrationPage.verifyTable();
     }
 
 }
